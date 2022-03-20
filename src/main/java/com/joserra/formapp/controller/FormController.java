b214b80aff43010327908248f24c398957df5984
joserra404 <201800035@alu.comillas.edu>
2022-03-21 00:12:43 +0100

    gitpod

--- src/main/java/com/joserra/formapp/controller/FormController.java
+++ src/main/java/com/joserra/formapp/controller/FormController.java
@@ -9,20 +9,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class FormController {
     ArrayList<FormData> datos = new ArrayList<FormData>();
 
     @GetMapping("/api/form")
     public @ResponseBody ResponseEntity<String> getForm() {
-
+        log.info("Se ha realiado una consulta");
         return ResponseEntity.ok().body(datos.toString());
     }
 
     @PostMapping("/api/form")
     public @ResponseBody ResponseEntity<String> postForm(@RequestBody FormData datosPost) {
         datos.add(datosPost);
-
+        log.info("Se ha recibido un nuevo mensaje");
+        
         return ResponseEntity.ok().body("Dato incluido");
     }
 }
